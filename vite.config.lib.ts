e043b4e1d0296beab17cce29638063df67de2dd4
Daniel Jonathan <daniel@cosmicverse.org>
2023-05-31 12:12:32 -0600

    feat(CM-250): updated build configurations

--- vite.config.lib.ts
+++ vite.config.lib.ts
@@ -69,6 +69,7 @@ export default defineConfig(() => ({
   },
   plugins,
   build: {
+    minify,
     emptyOutDir,
     lib: {
       name,
@@ -78,7 +79,8 @@ export default defineConfig(() => ({
     },
     rollupOptions: {
       external,
+      assetFileNames: (info): string =>
+        info.name || 'build-filename-undefined',
     },
-    minify,
   },
 }))
